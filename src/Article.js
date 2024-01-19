ee39b56c5ed0b4e5558358f1a128b3b792c9aa39
Tiago Alves <tiago.alves_2012@hotmail.com>
2024-01-19 11:20:28 -0300

    up

---
+++ src/Article.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Article extends React.Component{
+    render(){
+        return(
+            <div>
+            <h3>Título d artigo</h3>
+            <p>Conteudo</p>
+            </div>
+        )
+    }
+}
+
+export default Article
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <div>
+            <h3>Rodape</h3>
+            <h4>Rodape</p>
+            </div>
+        )
+    }
+}
+
+export default Footer
